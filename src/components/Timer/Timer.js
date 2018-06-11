904f2bf225436b9686a03e3a189685e3f659422c
odm275 <pomejia@gmail.com>
2018-06-10 21:40:37 -0500

    need timer to reset onSecond Click

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -7,7 +7,7 @@ import "./Timer.css";
 class Timer extends Component {
   constructor() {
     super();
-    this.counter = 0;
+    this.timerInProcess = false;
     this.state = {
       minutes: "0",
       seconds: "0"
@@ -15,29 +15,46 @@ class Timer extends Component {
   }
   componentDidMount() {
     this.props.getSettings();
+    console.log(this.props);
   }
+
+  stop() {}
+  start() {}
+  reset() {}
 
   startTimer(time) {
     let present = new Date().getTime();
-    const plus = parseInt(time, 10) * 1000 * 60;
+    const plus = (parseInt(time, 10) + 1) * 1000 * 60;
     const future = new Date(present + plus).getTime();
-    const interval = 1000;
-
+    const interval = 500;
     const t = setInterval(() => {
       present = new Date().getTime();
       let distance = future - present;
       let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+      if (distance < 0) {
+        clearInterval(t);
+        alert("Time is up!");
+        return;
+      }
       this.setState({
         minutes,
         seconds
       });
-      if (distance < 0) {
-        clearInterval(t);
-        alert("niggah!");
-      }
     }, interval);
+    if (this.timerInProcess) {
+      console.log("lol");
+      clearInterval(t);
+      this.timerInProcess = false;
+
+      return;
+    }
+    this.timerInProcess = true;
+  }
+  restart(t, time) {
+    clearInterval(t);
+    this.startTimer(time);
   }
 
   render() {
