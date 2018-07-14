5d9334e15b48b1d6766545d933ae4d55c6984372
odm275 <pomejia@gmail.com>
2018-07-14 14:07:48 -0500

    arrangeData clean

--- src/components/Graphics/Graphics.js
+++ src/components/Graphics/Graphics.js
@@ -10,6 +10,7 @@ class Graphics extends Component {
     this.state = {
       chartData: {}
     };
+    this.setRange = this.setRange.bind(this);
   }
   componentDidMount() {
     this.props.getCounts();
@@ -52,7 +53,7 @@ class Graphics extends Component {
     return (
       <div className="graphics">
         <h1>Lobster Stats</h1>
-        <DropDownInput range={timeRange} />
+        <DropDownInput range={timeRange} onClick={this.setRange} />
         <Chart
           chartData={this.state.chartData}
           displayTitle="Lobster Count"
@@ -73,57 +74,36 @@ export default connect(
 )(Graphics);
 
 function localStorageToChart(str) {
+  // String -> Array of Dates
   const arr = str.split(",");
   const toDates = arr.map(date => new Date(parseInt(date, 10)));
-  //plot: Arrays of arrays with each
-  //pomodoro occurances from each equivalent day
-  const dataFormat = arrangeData(toDates);
-  const fillDataFormat = fillInGaps(dataFormat);
-  return fillDataFormat;
+
+  return fillInGaps(arrangeData(toDates));
 }
-function fillInGaps(dataFormat) {
+// @desc [d1,d2,...,dN] -> [[d1,d2],[d3], [d4,d5,d6,d7]...Dn]
+// Array of dates -> Arrays of arrays of dates of the same day.
+function arrangeData(arr) {
+  const datesByDay = arr.reduce((acc, d1) => {
+    const pomDayCount = arr.filter(d2 => sameDay(d1, d2));
+    const length = acc.length;
+    if (length === 0 || !acc[length - 1].includes(d1)) {
+      acc.push(pomDayCount);
+      return acc;
+    }
+    return acc;
+  }, []);
+
+  return datesByDay;
+}
+
+function fillInGaps(arr) {
   const day = 1000 * 60 * 60 * 24;
-  let gap = [];
-  let dataFormatWGaps = [];
+  console.log(arr);
+  const arrWGaps = arr.reduce((acc, d1) => {
+    const day = d1[0];
+  }, []);
 
-  let countToFuture = (past, future, index) => {
-    if (sameDay(new Date(past), new Date(future))) {
-      insertGaps(gap, index);
-      gap = [];
-      return;
-    }
-    past += day;
-    gap.push(past); //Right here ... Should we push past before adding to past?
-    countToFuture(past, future, index);
-  };
-
-  let insertGaps = (gap, index) => {
-    //  Put gaps inside our dataFormat array
-    if (gap.length > 1) {
-      //  gap is going be between index and index+1 in the dataFormat array.
-      const slice1 = dataFormat.slice(0, index + 1);
-      const slice2 = dataFormat.slice(index + 1, dataFormat.length);
-      const gapMinusExtra = gap.slice(0, gap.length - 1);
-      dataFormatWGaps = slice1.concat(gapMinusExtra, slice2);
-    } else {
-      dataFormatWGaps = dataFormat;
-    }
-  };
-
-  let generateGaps = () => {
-    dataFormat.forEach((val, index) => {
-      //If we're not out of bounds
-      if (index + 1 < dataFormat.length) {
-        countToFuture(
-          dataFormat[index][0].getTime(),
-          dataFormat[index + 1][0].getTime(),
-          index
-        );
-      }
-    });
-  };
-  generateGaps();
-  return dataFormatWGaps;
+  return arr;
 }
 
 function generateLabels(data) {
@@ -155,31 +135,6 @@ function generateLabels(data) {
     const year = dateObject.getFullYear();
     return `${day}-${monthNames[monthIndex]}-${year}`;
   });
-}
-
-function arrangeData(arr) {
-  let result = [];
-  let array = arr;
-  let arrayLength;
-  let differenceLength;
-  array.forEach(d1 => {
-    const day = arr.reduce((acc, d2) => {
-      if (sameDay(d1, d2)) {
-        acc.push(d2);
-      }
-      return acc;
-    }, []);
-    arrayLength = array.length;
-    array = difference(array, day);
-    differenceLength = array.length;
-    if (arrayLength !== differenceLength) {
-      result.push(day);
-    }
-  });
-  return result;
-}
-function difference(arr1, arr2) {
-  return arr1.filter(x => !arr2.includes(x));
 }
 
 function sameDay(d1, d2) {
